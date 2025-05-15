04754b33b3d6f43a61f906f2e7d843bf718e8bff
anusha1808 <anushagupta1808@gmail.com>
2025-05-15 22:03:39 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -119,7 +119,6 @@ const App = () => (
             <main className="flex-grow">
               <AppRoutes />
             </main>
-            <Footer />
           </div>
         </BrowserRouter>
       </TooltipProvider>
